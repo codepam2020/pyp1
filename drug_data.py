2bba5a7895fe5f02efd375f433a775cf5573fc02
codePam <codepam2020@gmail.com>
2022-12-01 01:58:34 +0900

    final update

--- drug_data.py
+++ drug_data.py
@@ -1,212 +1,203 @@
 import pandas as pd
+
+
 class DrugData:
-  df = pd.read_csv('./productIngrName.csv')
+    df = pd.read_csv("./productIngrName.csv")
 
-  # 입력받은 약물이름을 csv파일 속 데이터에서 찾은 후 return
-  def getDrugName(self, drugName):
-    drugList = []
-    for i in range(len(self.df)):
-      if drugName in self.df['name'][i]:
-        drugList.append(self.df['name'][i])
-    return drugList
-  
-  
-  # 입력받은 약물이름을 주성분 코드로 return
-  def getDrugIngrCode(self, drugName):
-    ingrs = ''
-    for i in range(len(self.df)):
-      if drugName == self.df['name'][i]:
-        ingrs = self.df['ingrCode'][i]
-        break
-      
-    ingrsList = ingrs.split('///')
-    return ingrsList
-  
-  
-  # 입력받은 약물이름을 주성분 한국명으로 return
-  def getDrugIngrNameKor(self, drugName):
-    ingrs = ''
-    for i in range(len(self.df)):
-      if drugName == self.df['name'][i]:
-        ingrs = self.df['ingrNameKor'][i]
-        break
-      
-    ingrsList = ingrs.split('///')
-    return ingrsList
-  
-  
-  # 선택된 약물을 데이터베이스에 저장
-  def saveDrugDB(self, drugInfo):
-    try:
-      db = pd.read_csv('db.csv', index_col=0)
-      db = db.append(drugInfo, ignore_index=True)
-      db.to_csv('db.csv')
-    except:
-      db = drugInfo
-      db.to_csv('db.csv')
-  
-  
-  # 데이터베이스에서 복용 약물 리스트 return
-  def readDrugDB(self):
-    try:
-      db = pd.read_csv('db.csv', index_col=0)
-      return db
-    except:
-      pass
-    
-  # 데이터베이스에 db파일 덮어쓰기(선택 약물 삭제용)
-  def editDrugDB(self, newDB):
-    newDB.to_csv('db.csv')
-    
-    
-  # 병용금기 약물 검색 및 return
-  def getMixCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getUsjntTabooInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
-  
-  
-  # 특정 연령대 금기 약물 검색 및 return
-  def getSpecificAgeCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getSpcifyAgrdeTabooInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
+    # 입력받은 약물이름을 csv파일 속 데이터에서 찾은 후 return
+    def getDrugName(self, drugName):
+        drugList = []
+        for i in range(len(self.df)):
+            if drugName in self.df["name"][i]:
+                drugList.append(self.df["name"][i])
+        return drugList
 
-    
-  # 노인 주의 약물 검색 및 return
-  def getElderCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getOdsnAtentInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
-  
-  
-  # 임부 금기 약물 검색 및 return
-  def getPragnantCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getPwnmTabooInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
-  
-  
-  # 용량 주의 약물 검색 및 return
-  def getDoseCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getCpctyAtentInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
-  
-  
-  # 투여기간 주의 약물 검색 및 return
-  def getDayCautionDrug(self, ingrNameKor):
-    import requests
-    import json
-    
-    ingrNameKorShorten = ingrNameKor[:4]
-    
-    url = 'http://apis.data.go.kr/1471000/DURIrdntInfoService02/getMdctnPdAtentInfoList01?'
-    
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'pageNo': '1',
-      'type': 'json',
-      'numOfRows': '100',
-      'ingrKorName': ingrNameKorShorten
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    return json.loads(content)['body']['items']
-  
-  
-  # 약 상세정보 return
-  def getDetailedDrugInfo(self, name):
-    import requests
-    import xml.etree.ElementTree as ET
-    
-    url = 'http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService02/getDrugPrdtPrmsnDtlInq01'
-    params = {
-      'serviceKey': '4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==',
-      'type':'xml',
-      'pageNo': '1',
-      'numOfRows':'50',
-      'item_name': name,
-      }
-    
-    response = requests.get(url=url, params=params)
-    content = response.text
-    root = ET.fromstring(content)
+    # 입력받은 약물이름을 주성분 코드로 return
+    def getDrugIngrCode(self, drugName):
+        ingrs = ""
+        for i in range(len(self.df)):
+            if drugName == self.df["name"][i]:
+                ingrs = self.df["ingrCode"][i]
+                break
 
-    return root[1][3][0]
+        ingrsList = ingrs.split("///")
+        return ingrsList
+
+    # 입력받은 약물이름을 주성분 한국명으로 return
+    def getDrugIngrNameKor(self, drugName):
+        ingrs = ""
+        for i in range(len(self.df)):
+            if drugName == self.df["name"][i]:
+                ingrs = self.df["ingrNameKor"][i]
+                break
+
+        ingrsList = ingrs.split("///")
+        return ingrsList
+
+    # 선택된 약물을 데이터베이스에 저장
+    def saveDrugDB(self, drugInfo):
+        try:
+            db = pd.read_csv("db.csv", index_col=0)
+            db = db.append(drugInfo, ignore_index=True)
+            db.to_csv("db.csv")
+        except:
+            db = drugInfo
+            db.to_csv("db.csv")
+
+    # 데이터베이스에서 복용 약물 리스트 return
+    def readDrugDB(self):
+        try:
+            db = pd.read_csv("db.csv", index_col=0)
+            return db
+        except:
+            pass
+
+    # 데이터베이스에 db파일 덮어쓰기(선택 약물 삭제용)
+    def editDrugDB(self, newDB):
+        newDB.to_csv("db.csv")
+
+    # 병용금기 약물 검색 및 return
+    def getMixCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getUsjntTabooInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 특정 연령대 금기 약물 검색 및 return
+    def getSpecificAgeCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getSpcifyAgrdeTabooInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 노인 주의 약물 검색 및 return
+    def getElderCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getOdsnAtentInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 임부 금기 약물 검색 및 return
+    def getPragnantCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getPwnmTabooInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 용량 주의 약물 검색 및 return
+    def getDoseCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getCpctyAtentInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 투여기간 주의 약물 검색 및 return
+    def getDayCautionDrug(self, ingrNameKor):
+        import requests
+        import json
+
+        ingrNameKorShorten = ingrNameKor[:4]
+
+        url = "http://apis.data.go.kr/1471000/DURIrdntInfoService02/getMdctnPdAtentInfoList01?"
+
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "pageNo": "1",
+            "type": "json",
+            "numOfRows": "100",
+            "ingrName": ingrNameKorShorten,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        return json.loads(content)["body"]["items"]
+
+    # 약 상세정보 return
+    def getDetailedDrugInfo(self, name):
+        import requests
+        import xml.etree.ElementTree as ET
+
+        url = "http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService02/getDrugPrdtPrmsnDtlInq01"
+        params = {
+            "serviceKey": "4ARJOwbLh8jufyYInZFDNEp0phIdsR0d7ZZP0bqJKwTfQ3cL+Df7zJWkSnYAk+8+jCjn/V9RLSxZ2vNFQ+YHrQ==",
+            "type": "xml",
+            "pageNo": "1",
+            "numOfRows": "50",
+            "item_name": name,
+        }
+
+        response = requests.get(url=url, params=params)
+        content = response.text
+        root = ET.fromstring(content)
+
+        return root[1][3][0]
--- test.py
+++ test.py
@@ -3,27 +3,121 @@ from drug_data import DrugData
 import xml.etree.ElementTree as ET
 
 drugData = DrugData()
-drugName = '마그네스정'
 
-# ingrCode = drugData.getDrugIngrCode(drugName)
-# ingrNameKor = drugData.getDrugIngrNameKor(drugName)
-# drugInfo = pd.DataFrame({'name': drugName, 'ingrCode':[ingrCode], 'ingrNameKor': [ingrNameKor]})
+db = drugData.readDrugDB()
 
-res = drugData.getDetailedDrugInfo(drugName)
+# 병용금기 약물
+def findMixDrug():
+
+    cautionDrugList = []
+    cautionIngrList = []
+
+    for i in range(len(db)):
+        ingrNameKorList = eval(db["ingrNameKor"][i])
+        for ingrNameKor in ingrNameKorList:
+            try:
+                cautionList = drugData.getMixCautionDrug(ingrNameKor)
+                for item in cautionList:
+                    cautionIngrList.append(item["item"]["MIXTURE_ORI"])
+
+            except:
+                pass
+
+    for i in range(len(db)):
+        ingrCodeList = eval(db["ingrCode"][i])
+        for ingrCode in ingrCodeList:
+            for j in cautionIngrList:
+                if ingrCode in j:
+                    cautionDrugList.append(db["name"][i])
+                    break
+
+    cautionDrugList = list(set(cautionDrugList))
+
+    return "\n".join(cautionDrugList)
 
 
-name = res.find('EE_DOC_DATA').text
+# 특정 연령대 금기 약물
+def findSpecificAgeCautionDrug():
+    specificAgeCautionDrugList = []
 
-section = res.find('EE_DOC_DATA').find('DOC').find('SECTION')
+    for i in range(len(db)):
+        ingrList = eval(db["ingrNameKor"][i])
+        for item in ingrList:
+            try:
+                drugData.getSpecificAgeCautionDrug(item)[0]["item"]
+                specificAgeCautionDrugList.append(db["name"][i])
+            except:
+                pass
 
-efficacy = []
+    specificAgeCautionDrugList = list(set(specificAgeCautionDrugList))
 
-for tag in section:
-  efficacy.append(tag.get('title'))
-  for i in tag:
-    efficacy.append("  "+ i.text)
-    
+    return "\n".join(specificAgeCautionDrugList)
 
 
-print('\n'.join(efficacy))
+# 노인 복용 금기 약물
+def findElderCautionDrug():
+    elderCautionDrugList = []
+    for i in range(len(db)):
+        ingrList = eval(db["ingrNameKor"][i])
+        for item in ingrList:
+            try:
+                drugData.getElderCautionDrug(item)[0]["item"]
+                elderCautionDrugList.append(db["name"][i])
+            except:
+                pass
 
+    elderCautionDrugList = list(set(elderCautionDrugList))
+
+    return "\n".join(elderCautionDrugList)
+
+
+# 임부 복용 금기 약물
+def findPragnantCautionDrug():
+    pragnantCautionDrugList = []
+    for i in range(len(db)):
+        ingrList = eval(db["ingrNameKor"][i])
+        for item in ingrList:
+            try:
+                drugData.getPragnantCautionDrug(item)[0]["item"]
+                pragnantCautionDrugList.append(db["name"][i])
+            except:
+                pass
+
+    return "\n".join(pragnantCautionDrugList)
+
+
+# 용량 주의 약물
+def findDoseCautionDrug():
+    doseCautionDrugList = []
+    for i in range(len(db)):
+        ingrList = eval(db["ingrNameKor"][i])
+        for item in ingrList:
+            try:
+                drugData.getDoseCautionDrug(item)[0]["item"]
+                doseCautionDrugList.append(db["name"][i])
+            except:
+                pass
+
+    doseCautionDrugList = list(set(doseCautionDrugList))
+
+    return "\n".join(doseCautionDrugList)
+
+
+# 기간 주의 약물
+def findDayCautionDrug():
+    dayCautionDrugList = []
+    for i in range(len(db)):
+        ingrList = eval(db["ingrNameKor"][i])
+        for item in ingrList:
+            try:
+                drugData.getDayCautionDrug(item)[0]["item"]
+                dayCautionDrugList.append(db["name"][i])
+            except:
+                pass
+
+    dayCautionDrugList = list(set(dayCautionDrugList))
+
+    return "\n".join(dayCautionDrugList)
+
+
+print(findDayCautionDrug())
